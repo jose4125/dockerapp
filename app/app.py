e729faa6f2dbe0e87cee58bde9e2c25a329d4717
jose4125 <jose4125@gmail.com>
2020-01-15 00:27:51 -0500

    add prived aws ec2 host address

--- app/app.py
+++ app/app.py
@@ -23,4 +23,4 @@ def mainpage():
 	return render_template('index.html', key=key, cache_value=cache_value)
 
 if __name__ == '__main__':
-    app.run(host='35.167.212.133')
+    app.run(host='172.31.19.38')
